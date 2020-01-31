42db06c56772a66b6a5a7837c24a6e95f87cc3be
Анисимов Алексей <a.anisimov@info-link.ru>
2020-01-31 15:55:52 +0300

    server test

--- serv/main.go
+++ serv/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"net"
 	"context"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	pb "github.com/AlekseiAnisimov/go-todo-user/proto"
 )
 
 type server struct {
 }
 
-/*func (s *server) Check(ctx context.Context, in ) () {
+func (s *server) Check(ctx context.Context, in *pb.Request) (*pb.Response, error) {
+	var test *string
+	return &pb.Response{Message: test}, nil
+}
 
-}*/
+func main() {
+	listener, err := net.Listen("tcp", ":20100")
+	if err != nil {
+		fmt.Print("error 1")
+	}
+	fmt.Print("start")
+
+	rpcserv := grpc.NewServer()
+
+	pb.RegisterBearerAuthServer(rpcserv, &server{})
+	reflection.Register(rpcserv)
+
+	err = rpcserv.Serve(listener)
+	if err != nil {
+		fmt.Print("error 2")
+	}
+}
